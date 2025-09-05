288eb9002b9f6f315e4881cb722b3d32b04ce401
AZR437 <106365725+AZR437@users.noreply.github.com>
2025-09-05 09:34:46 +0800

    lib

--- SDLTest/include/Runner.h
+++ SDLTest/include/Runner.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "SDL3/SDL.h"
+class Runner
+{
+private:
+	static SDL_Window* window;
+	static SDL_Renderer* renderer;
+public:
+	Runner();
+	~Runner();
+
+
+};
+
+Runner::Runner()
+{
+}
+
+Runner::~Runner()
+{
+}

c103678aab278081350886f1d43fb92e8b6195d9
AZR437 <106365725+AZR437@users.noreply.github.com>
2025-09-05 10:02:25 +0800

    update on main

---
+++ SDLTest/src/main.cpp
@@ -0,0 +1,15 @@
+#include "SDL3/SDL.h"
+#include <memory>
+
+
+int main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO) != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init failed (%s)", SDL_GetError());
+    }
+
+    if (SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello World",
+        "!! Your SDL project successfully runs on Android !!", NULL) != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_ShowSimpleMessageBox failed (%s)", SDL_GetError());
+    }
+}

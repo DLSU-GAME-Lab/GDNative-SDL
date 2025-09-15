32c64be45cb58d8f93b23ec8dbeaf375c87d77a5
AZR437 <106365725+AZR437@users.noreply.github.com>
2025-09-15 21:53:37 +0800

    Update Title_Scene.cpp

--- SDLTest/src/Title_Scene.cpp
+++ SDLTest/src/Title_Scene.cpp
@@ -23,6 +23,8 @@ void Title_Scene::onLoadResources()
     TextureManager::getInstance()->load("title_screen_pngs/SP_Gem_Red.png", "Red");
     TextureManager::getInstance()->load("title_screen_pngs/SP_Gem_White.png", "White");
     TextureManager::getInstance()->load("title_screen_pngs/SP_Gem_Green.png", "Green");
+    TextureManager::getInstance()->load("title_screen_pngs/Sprite_Window.png", "Button_Container");
+    TextureManager::getInstance()->load("title_screen_pngs/Sprite_LevelEntry_Bg.png", "Level_Button");
 }
 
 void Title_Scene::onLoadObjects()
@@ -36,18 +38,34 @@ void Title_Scene::onLoadObjects()
     Prop* pRed = new Prop("Red", "Red", Vector2D(350.f, 250.f), Vector2D(.35f, .35f), 0.0f, false);
     Prop* pWhite = new Prop("White", "White", Vector2D(500.f, 100.f), Vector2D(.35f, .35f), 0.0f, false);
     Prop* pGreen = new Prop("Green", "Green", Vector2D(1300.f, 500.f), Vector2D(.35f, .35f), 0.0f, false);
+    Prop* pLevelSelect = new Prop("Level_Select", "Button_Container", Vector2D(350, 750.f), Vector2D(7, 3), 0.0f, false);
 
     // create a button that switches to the Lobby scene when clicked
-    //UIButton* pStartButton = new UIButton(
-    //    "Start_Button_Object",   // object name (can be unique)
-    //    "Start_Button",          // texture key (MUST match TextureManager)
-    //    250.f, 600.f,
-    //    0.25f, 0.25f,
-    //    0.0f, false,
-    //    SceneTag::LOBBY_SCENE
-    //);
+    UIButton* pLevelOne = new UIButton(
+        "Level_1_Button",   // object name (can be unique)
+        "Level_Button",          // texture key (MUST match TextureManager)
+        Vector2D(550.f, 825.f),
+        Vector2D(2.f,2.f),
+        0.0f, false,
+        SceneTag::LOBBY_SCENE
+    );
+    UIButton* pLevelTwo = new UIButton(
+        "Level_2_Button",   // object name (can be unique)
+        "Level_Button",          // texture key (MUST match TextureManager)
+        Vector2D(900.f, 825.f),
+        Vector2D(2.f, 2.f),
+        0.0f, false,
+        SceneTag::LOBBY_SCENE
+    );
+    UIButton* pLevelThree = new UIButton(
+        "Level_3_Button",   // object name (can be unique)
+        "Level_Button",          // texture key (MUST match TextureManager)
+        Vector2D(1250.f, 825.f),
+        Vector2D(2.f, 2.f),
+        0.0f, false,
+        SceneTag::LOBBY_SCENE
+    );
 
-    //GameObjectManager::getInstance()->addObject((AGameObject*)pStartButton);
     GameObjectManager::getInstance()->addObject((AGameObject*)pBackground);
     GameObjectManager::getInstance()->addObject((AGameObject*)pLogo);
     GameObjectManager::getInstance()->addObject((AGameObject*)pYellow);
@@ -56,6 +74,11 @@ void Title_Scene::onLoadObjects()
     GameObjectManager::getInstance()->addObject((AGameObject*)pRed);
     GameObjectManager::getInstance()->addObject((AGameObject*)pWhite);
     GameObjectManager::getInstance()->addObject((AGameObject*)pGreen);
+    GameObjectManager::getInstance()->addObject((AGameObject*)pLevelSelect);
+    GameObjectManager::getInstance()->addObject((AGameObject*)pLevelOne);
+    GameObjectManager::getInstance()->addObject((AGameObject*)pLevelTwo);
+    GameObjectManager::getInstance()->addObject((AGameObject*)pLevelThree);
+
 }
 
 void Title_Scene::onUnloadResources()
@@ -68,6 +91,8 @@ void Title_Scene::onUnloadResources()
     TextureManager::getInstance()->unload("Purple");
     TextureManager::getInstance()->unload("Red");
     TextureManager::getInstance()->unload("White");
+    TextureManager::getInstance()->unload("Button_Container");
+    TextureManager::getInstance()->unload("Level_Button");
 }
 
 void Title_Scene::onUnloadObjects()

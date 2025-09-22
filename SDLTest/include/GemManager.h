0c805f1942ef2b095c244e4621f0f30c8c32ae68
Vaughn <44435027+blazev01@users.noreply.github.com>
2025-09-22 11:08:29 +0800

    gem manager WIP

--- SDLTest/include/GemManager.h
+++ SDLTest/include/GemManager.h
@@ -1,15 +1,29 @@
 #pragma once
 #include "AComponent.h"
+#include "Vector2D.h"
 #include <vector>
 
 class GemManager : public AComponent
 {
+public:
+    struct CellData
+    {
+        Uint8 c;
+        Uint8 r;
+        Vector2D pos;
+        AGameObject* obj;
+    };
 private:
+    int selected[2] = { -1, -1 };
     std::vector<AGameObject*> vecGem;
+    std::vector<Vector2D> vecTile;
+    float fCellSize;
 
 public:
     void perform() override;
     void startLevel();
+
+    AGameObject* getGem();
     void moveGems();
     void matchGems();
 
--- SDLTest/src/GemManager.cpp
+++ SDLTest/src/GemManager.cpp
@@ -9,6 +9,26 @@ void GemManager::perform()
 
 void GemManager::startLevel()
 {
+    for (int i = 0; i < this->vecTile.size(); i++)
+    {
+        //TODO: create random gem
+        this->vecGem.push_back(NULL);
+    }
+}
+
+void GemManager::moveGems()
+{
+    Vector2D pos = vecGem[selected[0]]->getPos();
+    vecGem[selected[0]]->setPos(vecGem[selected[1]]->getPos());
+    vecGem[selected[1]]->setPos(pos);
+    selected[0] = -1;
+    selected[1] = -1;
+
+    matchGems();
+}
+
+void GemManager::matchGems()
+{
 
 }
 

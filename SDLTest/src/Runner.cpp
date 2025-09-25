d1be187feb9a57ece788abb022c65b8e38b75a90
Vaughn <44435027+blazev01@users.noreply.github.com>
2025-09-25 11:54:10 +0800

    merge error fixes

--- SDLTest/src/Runner.cpp
+++ SDLTest/src/Runner.cpp
@@ -26,10 +26,10 @@
 #include "imgui_impl_sdlrenderer3.h"
 
 // scenes
-#include "LobbyScene.h"
 #include "Title_Scene.h"
-#include "RightRoomScene.h"
-#include "LeftRoomScene.h"
+#include "Level_1_Scene.h"
+#include "Level_2_Scene.h"
+#include "Level_3_Scene.h"
 
 Runner::Runner()
 {
@@ -60,7 +60,7 @@ Runner::Runner()
 	// debug
 	std::cout << "Screen size: " << screenWidth << "x" << screenHeight << std::endl;
 
-	this->strWindowTitle = "Babaylan Tales";
+	this->strWindowTitle = "Gem Hunter Match";
 	float scaleX = (float)screenWidth / gameWidth;
 	float scaleY = (float)screenHeight / gameHeight;
 	float scale = (std::min)(scaleX, scaleY);
@@ -231,15 +231,16 @@ void Runner::render()
 void Runner::registerScenes()
 {
 	auto titleScene = new Title_Scene();
-	auto lobbyScene = new LobbyScene();
-	auto rightRoomScene = new RightRoomScene();
-	auto leftRoomScene = new LeftRoomScene();
+	auto level1Scene = new Level_1_Scene();
+	auto level2Scene = new Level_2_Scene();
+	auto level3Scene = new Level_3_Scene();
 
 	SceneManager::getInstance()->registerScene(titleScene);
-	SceneManager::getInstance()->registerScene(lobbyScene);
-	SceneManager::getInstance()->registerScene(rightRoomScene);
-	SceneManager::getInstance()->registerScene(leftRoomScene);
+	SceneManager::getInstance()->registerScene(level1Scene);
+	SceneManager::getInstance()->registerScene(level2Scene);
+	SceneManager::getInstance()->registerScene(level3Scene);
 
 	//load initial scene
 	SceneManager::getInstance()->loadScene(SceneTag::TITLE_SCENE);
+
 }

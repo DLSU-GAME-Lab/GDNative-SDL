220161b17f2f23bf8f80e9fef5d626e7b05f0bf8
AZR437 <106365725+AZR437@users.noreply.github.com>
2025-09-26 14:35:58 +0800

    Title

--- SDLTest/src/Title_Scene.cpp
+++ SDLTest/src/Title_Scene.cpp
@@ -1,6 +1,5 @@
 #include "Title_Scene.h"
 #include "Background.h"
-#include "UIButton.h"
 #include "GameObjectManager.h"
 #include "TextureManager.h"
 #include "EnumSceneTag.h"
@@ -10,6 +9,7 @@
 #include "Text.h"
 #include "GUIButton.h"
 #include "SceneSwitcher.h"
+#include "Prop.h"
 
 Title_Scene::Title_Scene()
     : AScene(SceneTag::TITLE_SCENE)
@@ -18,10 +18,59 @@ Title_Scene::Title_Scene()
 
 void Title_Scene::onLoadResources()
 {
-    // naming convention (TextureManager prefixes "Assets/")
-    TextureManager::getInstance()->load("title_screen_pngs/title_screen_bg.png", "Title_Background");
-    TextureManager::getInstance()->load("title_screen_pngs/title_button.png", "Start_Button");
+    this->loadText();
+    this->loadAnimatedTextures();
+    this->loadSceneTextures();
+}
 
+void Title_Scene::onLoadObjects()
+{
+    // create and register game objects for the title scene
+    this->createScene();
+    this->createButtons();
+
+    //// create a button that switches to the Lobby scene when clicked
+    //UIButton* pStartButton = new UIButton(
+    //    "Start_Button_Object",   // object name (can be unique)
+    //    "Start_Button",          // texture key (MUST match TextureManager)
+    //    Vector2D(-580.f, 15.f),
+    //    Vector2D(0.25f, 0.25f),
+    //    0.0f, false,
+    //    SceneTag::LOBBY_SCENE
+    //);
+    //GameObjectManager::getInstance()->addObject((AGameObject*)pStartButton);
+
+
+
+
+}
+
+void Title_Scene::onUnloadResources()
+{
+    TextureManager::getInstance()->unload("Title_Background");
+    TextureManager::getInstance()->unload("Start_Button");
+    TextureManager::getInstance()->unload("Player");
+    TextureManager::getInstance()->unload("Librarian");
+    TextureManager::getInstance()->unload("Fairy");
+    TextureManager::getInstance()->unload("Start");
+    TextureManager::getInstance()->unload("Title_Banner");
+    TextureManager::getInstance()->unload("DLSU_Logos");
+    FontManager::getInstance()->unloadFont("LazyFont");
+}
+
+void Title_Scene::onUnloadObjects()
+{
+    // base implementation to remove all objects
+    AScene::onUnloadObjects();
+}
+
+void Title_Scene::loadText()
+{
+    FontManager::getInstance()->loadFont("lazy.ttf", "LazyFont", 90);
+    TextureManager::getInstance()->loadFromText("Start", "LazyFont", "Start Game", colorBlack);
+}
+void Title_Scene::loadAnimatedTextures()
+{
     for (int i = 0; i < 6; i++)
     {
         std::string strPath = "animations/title_scene/player/Set" + std::to_string(i) + ".png";
@@ -39,16 +88,36 @@ void Title_Scene::onLoadResources()
         std::string strPath = "animations/title_scene/librarian/Set" + std::to_string(i) + ".png";
         TextureManager::getInstance()->load(strPath, "Librarian");
     }
-    FontManager::getInstance()->loadFont("lazy.ttf", "LazyFont", 90);
-    TextureManager::getInstance()->loadFromText("Start", "LazyFont", "Start Game", colorBlack);
+}
 
+void Title_Scene::loadSceneTextures()
+{
+    TextureManager::getInstance()->load("title_screen_pngs/title_screen_bg.png", "Title_Background");
+    TextureManager::getInstance()->load("title_screen_pngs/title_button.png", "Start_Button");
+    TextureManager::getInstance()->load("title_screen_pngs/game_logo.png", "Title_Banner");
+    TextureManager::getInstance()->load("title_screen_pngs/DLSU_logos.png", "DLSU_Logos");
+}
+
+void Title_Scene::createButtons()
+{
+    GUIButton* pStartButton = new GUIButton("Start_Button", "Start_Button");
+    //GUIUtils::setGUIMidLeft(pStartButton, Vector2D(20.0f, 0.0f));
+    pStartButton->setPos(Vector2D(0, 0));
+    pStartButton->setScale(Vector2D(0.25f, 0.25f));
+
+    SceneSwitcher* pSceneSwitcher = new SceneSwitcher(SceneTag::LOBBY_SCENE);
+    pStartButton->attachComponent(pSceneSwitcher);
+    GameObjectManager::getInstance()->addObject(pStartButton);
+    Text* pStartText = new Text("Start_Text", "Start", Vector2D(0, 0), Vector2D(1, 1), 0.f, false);
+    pStartButton->attachChild(pStartText);
+    GameObjectManager::getInstance()->addObject(pStartText);
+    pStartButton->setPos(Vector2D(-580, -150));
 
 }
 
-void Title_Scene::onLoadObjects()
-{
-    // create and register game objects for the title scene
-    Background* pBackground = new Background("Title_Background", "Title_Background", Vector2D(1.f,1.f));
+void Title_Scene::createScene()
+{ 
+    Background* pBackground = new Background("Title_Background", "Title_Background", Vector2D(1.f, 1.f));
     GameObjectManager::getInstance()->addObject(pBackground);
 
     AnimatedSprite* pLibrarian = new AnimatedSprite("Librarian", "Librarian", Vector2D(-50.f, -240), Vector2D(1.f, 1.f), 0.f, 8);
@@ -60,43 +129,10 @@ void Title_Scene::onLoadObjects()
     AnimatedSprite* pPlayer = new AnimatedSprite("Player", "Player", Vector2D(-300, 60.f), Vector2D(1.f, 1.f), 0.f, 8);
     GameObjectManager::getInstance()->addObject(pPlayer);
 
-    //// create a button that switches to the Lobby scene when clicked
-    //UIButton* pStartButton = new UIButton(
-    //    "Start_Button_Object",   // object name (can be unique)
-    //    "Start_Button",          // texture key (MUST match TextureManager)
-    //    Vector2D(-580.f, 15.f),
-    //    Vector2D(0.25f, 0.25f),
-    //    0.0f, false,
-    //    SceneTag::LOBBY_SCENE
-    //);
-    //GameObjectManager::getInstance()->addObject((AGameObject*)pStartButton);
+    Prop* pLogo = new Prop("Game_Logo", "Title_Banner", Vector2D(-550, 200), Vector2D(1.f, 1.f), 0.f, false);
+    GameObjectManager::getInstance()->addObject(pLogo);
 
-    GUIButton* pStartButton = new GUIButton("Start_Button", "Start_Button");
-    //GUIUtils::setGUIMidLeft(pStartButton, Vector2D(20.0f, 0.0f));
-    pStartButton->setPos(Vector2D(-580.f, 15.f));
-    pStartButton->setScale(Vector2D(0.25f, 0.25f));
+    Prop* pDLSULogo = new Prop("DLSU_Logos", "DLSU_Logos", Vector2D(580, -420), Vector2D(.75f, .75f), 0.f, false);
+    GameObjectManager::getInstance()->addObject(pDLSULogo);
 
-    SceneSwitcher* pSceneSwitcher = new SceneSwitcher(SceneTag::LOBBY_SCENE);
-    pStartButton->attachComponent(pSceneSwitcher);
-    GameObjectManager::getInstance()->addObject(pStartButton);
-
-    Text* pStartText = new Text("Start_Text", "Start", Vector2D(-573, 22), Vector2D(1, 1), 0.f, false);
-    GameObjectManager::getInstance()->addObject(pStartText);
-}
-
-void Title_Scene::onUnloadResources()
-{
-    TextureManager::getInstance()->unload("Title_Background");
-    TextureManager::getInstance()->unload("Start_Button");
-    TextureManager::getInstance()->unload("Player");
-    TextureManager::getInstance()->unload("Librarian");
-    TextureManager::getInstance()->unload("Fairy");
-    TextureManager::getInstance()->unload("Start");
-    FontManager::getInstance()->unloadFont("LazyFont");
-}
-
-void Title_Scene::onUnloadObjects()
-{
-    // base implementation to remove all objects
-    AScene::onUnloadObjects();
 }

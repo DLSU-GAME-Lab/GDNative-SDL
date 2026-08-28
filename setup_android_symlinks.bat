d548fcba15c6739a88e4e20e5153b1650f7ffbe0
Vaughn <vaughnvc@yahoo.com>
2026-08-28 22:57:07 +0800

    Fixed setup_android_symlinks.bat

--- setup_android_symlinks.bat
+++ setup_android_symlinks.bat
@@ -1,14 +1,21 @@
 @echo off
-cd /d %~dp0android-project\app\jni
+setlocal
+
+set "ROOT=%~dp0"
+set "JNI=%ROOT%android-project\app\jni"
+
+cd /d "%JNI%"
 
 echo Removing old directories...
 
-if exist src (rmdir /S /Q src)
-if exist include (rmdir /S /Q include)
+if exist "src" rmdir /S /Q "src"
+if exist "include" rmdir /S /Q "include"
 
 echo Creating symbolic links...
-mklink /D src ..\..\..\SDLTest\src
-mklink /D include ..\..\..\SDLTest\include
 
+mklink /D "src" "%ROOT%SDLTest\src"
+mklink /D "include" "%ROOT%SDLTest\include"
+
+echo.
 echo Done.
 pause
